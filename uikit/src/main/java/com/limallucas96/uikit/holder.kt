3c87535ca11c346d42e6f12644df02d3f295458c
gl-lucaslima <lucas.silva@greenlight.me>
2022-08-13 18:51:37 -0300

    crated ui kit

---
+++ uikit/src/main/java/com/limallucas96/uikit/holder.kt
@@ -0,0 +1,1 @@
+package com.limallucas96.uikit
---
+++ uikit/src/main/java/com/limallucas96/uikit/views/MyButton.kt
@@ -0,0 +1,13 @@
+package com.limallucas96.uikit.views
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatButton
+
+class MyButton @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null,
+    defStyleRes: Int = 0
+) : AppCompatButton(context, attributeSet, defStyleRes) {
+
+}
