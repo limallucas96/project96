03e89becabdd938777a2e0d4c5fa91d37deafed7
gl-lucaslima <lucas.silva@greenlight.me>
2022-08-29 15:15:48 -0300

    added tests for CatProfileViewModel

---
+++ feature-one/src/test/java/com/limallucas96/feature_one/cat/CatProfileViewModelTest.kt
@@ -0,0 +1,70 @@
+package com.limallucas96.feature_one.cat
+
+import com.limallucas96.feature_one.base.BaseMVIViewModelTest
+import com.limallucas96.feature_one.catprofile.CatProfileAction
+import com.limallucas96.feature_one.catprofile.CatProfileSideEffect
+import com.limallucas96.feature_one.catprofile.CatProfileViewModel
+import com.limallucas96.feature_one.catprofile.CatProfileViewState
+import org.junit.Test
+
+class CatProfileViewModelTest :
+    BaseMVIViewModelTest<CatProfileAction, CatProfileViewState, CatProfileSideEffect, CatProfileViewModel>() {
+
+    private val mockCatProfileViewState = CatProfileViewState(
+        catName = "",
+        catAge = "",
+        isContinueButtonEnable = false
+    )
+
+    override fun getViewModel(): CatProfileViewModel {
+        return CatProfileViewModel()
+    }
+
+    @Test
+    fun `When only cat name is populated, then assert that isContinueButtonEnable is false`() =
+        assertViewState(
+            expectedViewState = mockCatProfileViewState.copy(
+                catName = "Cat",
+                catAge = "",
+                isContinueButtonEnable = false
+            ),
+            actions = listOf(CatProfileAction.OnCatNameChanged(catName = "Cat"))
+        )
+
+    @Test
+    fun `When only cat age is populated, then assert that isContinueButtonEnable is false`() =
+        assertViewState(
+            expectedViewState = mockCatProfileViewState.copy(
+                catName = "",
+                catAge = "2",
+                isContinueButtonEnable = false
+            ),
+            actions = listOf(CatProfileAction.OnCatAgeChanged(catAge = "2"))
+        )
+
+    @Test
+    fun `When both cat name and age are populated, then assert that isContinueButtonEnable is true`() =
+        assertViewState(
+            expectedViewState = mockCatProfileViewState.copy(
+                catName = "Cat",
+                catAge = "2",
+                isContinueButtonEnable = true
+            ),
+            actions = listOf(
+                CatProfileAction.OnCatNameChanged(catName = "Cat"),
+                CatProfileAction.OnCatAgeChanged(catAge = "2")
+            )
+        )
+
+    @Test
+    fun `When ButtonContinueClick is dispatched, then assert that NavigateToCatPicker is emitted`() =
+        assertSideEffect(
+            expectedSideEffect = CatProfileSideEffect.NavigateToCatPicker(
+                catName = "",
+                catAge = "",
+                backStack = "CAT_PROFILE_BACK_STACK"
+            ),
+            actions = listOf(CatProfileAction.ButtonContinueClick)
+        )
+
+}
